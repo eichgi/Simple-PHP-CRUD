def2ca48adcf6b250a0eecc3db396878b52f71b5
Hiram Guerrero <eichgi@users.noreply.github.com>
2017-03-31 08:41:57 -0600

    Update ApiClass.php

--- php/ApiClass.php
+++ php/ApiClass.php
@@ -10,7 +10,7 @@ class ApiClass
     {
         try {
 
-            $pdo = new PDO("mysql:host=localhost;dbname=crud", "root", "4lt4m1r4*");
+            $pdo = new PDO("mysql:host=localhost;dbname=crud", "user", "password");
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             $pdo->exec("set charset utf8");
