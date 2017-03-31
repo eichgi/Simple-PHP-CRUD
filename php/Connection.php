41a33003e7e704d93b20d6d156c446d1a4919fba
Hiram Guerrero <eichgi@users.noreply.github.com>
2017-03-31 08:42:27 -0600

    Update Connection.php

--- php/Connection.php
+++ php/Connection.php
@@ -3,7 +3,7 @@ require('FluentPDO/FluentPDO.php');
 
 try {
 
-    $pdo = new PDO("mysql:host=localhost;dbname=blog", "root", "4lt4m1r4*");
+    $pdo = new PDO("mysql:host=localhost;dbname=blog", "user", "password");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->exec("set charset utf8");
